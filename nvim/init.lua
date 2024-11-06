37926c4cdde38a6c3feb4c7e4b3d3cd19dd6f7a8
nellaG <seirios0107@gmail.com>
2024-11-07 00:04:32 +0900

    apply stylua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,5 @@
 vim.g.base46_cache = vim.fn.stdpath "data" .. "/base46/"
-vim.g.mapleader = "\\"  -- set leader key
+vim.g.mapleader = "\\" -- set leader key
 
 -- bootstrap lazy and all plugins
 local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
@@ -28,28 +28,51 @@ require("lazy").setup({
 --#region custom config start
 --
 -- Prevent line wrap
-vim.opt.whichwrap:remove("<")
-vim.opt.whichwrap:remove(">")
+vim.opt.whichwrap:remove "<"
+vim.opt.whichwrap:remove ">"
 vim.opt.colorcolumn = { 80, 120 }
 
 -- Run gofmt + goimports on save
-local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
 vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*.go",
-  callback = function()
-   require('go.format').gofmt()
+  pattern = "*",
+  callback = function(args)
+    require("conform").format { bufnr = args.buf }
   end,
-  group = format_sync_grp,
 })
 
 -- nvim-treesitter parser install config
-require'nvim-treesitter.configs'.setup {
+require("nvim-treesitter.configs").setup {
   -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-  ensure_installed = { "lua", "typescript", "yaml", "toml", "tmux", "terraform", "python", "json",
-  "json5", "html", "gomod", "go", "goctl", "dockerfile", "csv", "bash", "fish", "vim",
-  "vimdoc", "query", "markdown", "markdown_inline",
-  "git_config", "git_rebase", "gitattributes", "gitcommit", "gitignore", "nginx",
-  "requirements",
+  ensure_installed = {
+    "lua",
+    "typescript",
+    "yaml",
+    "toml",
+    "tmux",
+    "terraform",
+    "python",
+    "json",
+    "json5",
+    "html",
+    "gomod",
+    "go",
+    "goctl",
+    "dockerfile",
+    "csv",
+    "bash",
+    "fish",
+    "vim",
+    "vimdoc",
+    "query",
+    "markdown",
+    "markdown_inline",
+    "git_config",
+    "git_rebase",
+    "gitattributes",
+    "gitcommit",
+    "gitignore",
+    "nginx",
+    "requirements",
   },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
@@ -60,26 +83,25 @@ require'nvim-treesitter.configs'.setup {
   auto_install = true,
 }
 
-
-require('gitsigns').setup {
+require("gitsigns").setup {
   signs = {
-    add          = { text = '✚' },
-    change       = { text = '┃' },
-    delete       = { text = '_' },
-    topdelete    = { text = '‾' },
-    changedelete = { text = '~' },
-    untracked    = { text = '┆' },
+    add = { text = "✚" },
+    change = { text = "┃" },
+    delete = { text = "_" },
+    topdelete = { text = "‾" },
+    changedelete = { text = "~" },
+    untracked = { text = "┆" },
   },
   signs_staged = {
-    add          = { text = '✚' },
-    change       = { text = '┃' },
-    delete       = { text = '_' },
-    topdelete    = { text = '‾' },
-    changedelete = { text = '~' },
-    untracked    = { text = '┆' },
+    add = { text = "✚" },
+    change = { text = "┃" },
+    delete = { text = "_" },
+    topdelete = { text = "‾" },
+    changedelete = { text = "~" },
+    untracked = { text = "┆" },
   },
   on_attach = function(bufnr)
-    local gitsigns = require('gitsigns')
+    local gitsigns = require "gitsigns"
 
     local function map(mode, l, r, opts)
       opts = opts or {}
@@ -88,42 +110,49 @@ require('gitsigns').setup {
     end
 
     -- Navigation
-    map('n', ']c', function()
+    map("n", "]c", function()
       if vim.wo.diff then
-        vim.cmd.normal({']c', bang = true})
+        vim.cmd.normal { "]c", bang = true }
       else
-        gitsigns.nav_hunk('next')
+        gitsigns.nav_hunk "next"
       end
     end)
 
-    map('n', '[c', function()
+    map("n", "[c", function()
       if vim.wo.diff then
-        vim.cmd.normal({'[c', bang = true})
+        vim.cmd.normal { "[c", bang = true }
       else
-        gitsigns.nav_hunk('prev')
+        gitsigns.nav_hunk "prev"
       end
     end)
 
     -- Actions
-    map('n', '<leader>hs', gitsigns.stage_hunk)
-    map('n', '<leader>hr', gitsigns.reset_hunk)
-    map('v', '<leader>hs', function() gitsigns.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
-    map('v', '<leader>hr', function() gitsigns.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
-    map('n', '<leader>hS', gitsigns.stage_buffer)
-    map('n', '<leader>hu', gitsigns.undo_stage_hunk)
-    map('n', '<leader>hR', gitsigns.reset_buffer)
-    map('n', '<leader>hp', gitsigns.preview_hunk_inline)
-    map('n', '<leader>hb', function() gitsigns.blame_line{full=true} end)
-    map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
-    map('n', '<leader>hd', gitsigns.diffthis)
-    map('n', '<leader>hD', function() gitsigns.diffthis('~') end)
-    map('n', '<leader>td', gitsigns.toggle_deleted)
+    map("n", "<leader>hs", gitsigns.stage_hunk)
+    map("n", "<leader>hr", gitsigns.reset_hunk)
+    map("v", "<leader>hs", function()
+      gitsigns.stage_hunk { vim.fn.line ".", vim.fn.line "v" }
+    end)
+    map("v", "<leader>hr", function()
+      gitsigns.reset_hunk { vim.fn.line ".", vim.fn.line "v" }
+    end)
+    map("n", "<leader>hS", gitsigns.stage_buffer)
+    map("n", "<leader>hu", gitsigns.undo_stage_hunk)
+    map("n", "<leader>hR", gitsigns.reset_buffer)
+    map("n", "<leader>hp", gitsigns.preview_hunk_inline)
+    map("n", "<leader>hb", function()
+      gitsigns.blame_line { full = true }
+    end)
+    map("n", "<leader>tb", gitsigns.toggle_current_line_blame)
+    map("n", "<leader>hd", gitsigns.diffthis)
+    map("n", "<leader>hD", function()
+      gitsigns.diffthis "~"
+    end)
+    map("n", "<leader>td", gitsigns.toggle_deleted)
 
     -- Text object
-    map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
-  end
+    map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+  end,
 }
-
 
 -- lsp go to definition setting
 vim.api.nvim_create_autocmd("LspAttach", {
@@ -136,30 +165,28 @@ vim.api.nvim_create_autocmd("LspAttach", {
 })
 
 -- ibl setup
-require('ibl').setup {
+require("ibl").setup {
   scope = {
     enabled = true,
     show_start = true,
     show_end = false,
     show_exact_scope = true,
-
-  }
+  },
 }
-
 
 -- delimiter and indent line colorization
 local highlight = {
-    "RainbowRed",
-    "RainbowYellow",
-    "RainbowBlue",
-    "RainbowOrange",
-    "RainbowGreen",
-    "RainbowViolet",
-    "RainbowCyan",
+  "RainbowRed",
+  "RainbowYellow",
+  "RainbowBlue",
+  "RainbowOrange",
+  "RainbowGreen",
+  "RainbowViolet",
+  "RainbowCyan",
 }
-  local hooks = require "ibl.hooks"
-  -- create the highlight groups in the highlight setup hook, so they are reset
-  -- every time the colorscheme changes
+local hooks = require "ibl.hooks"
+-- create the highlight groups in the highlight setup hook, so they are reset
+-- every time the colorscheme changes
 hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
   vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
   vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
@@ -170,23 +197,21 @@ hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
   vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
 end)
 
-  require("ibl").setup {
-    scope = {
-      highlight = highlight,
-      show_start = false,
-      show_end = false,
-      show_exact_scope = true,
-    },
-    indent = {
+require("ibl").setup {
+  scope = {
     highlight = highlight,
-    },
-  }
+    show_start = false,
+    show_end = false,
+    show_exact_scope = true,
+  },
+  indent = {
+    highlight = highlight,
+  },
+}
 
-  hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
-
+hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
 
 -- custom config end
-
 
 -- load theme
 dofile(vim.g.base46_cache .. "defaults")
