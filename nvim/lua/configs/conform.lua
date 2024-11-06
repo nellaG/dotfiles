01763e9c7f32467eeb1a5cb79ff9d5bccca2b71d
nellaG <seirios0107@gmail.com>
2024-11-07 00:04:07 +0900

    setup conform.nvim to organize formatting

--- nvim/lua/configs/conform.lua
+++ nvim/lua/configs/conform.lua
@@ -1,15 +1,19 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    python = { "ruff_format", "ruff_organize_imports" },
+    go = { "gofumpt" },
+    typescript = { "prettier" },
+    typescriptreact = { "prettier" },
+    bash = { "shfmt" },
+    fish = { "fish_indent" },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    -- These options will be passed to conform.format()
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 }
 
-return options
+require("conform").setup(options)
